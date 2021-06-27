452b3395d8e001fcacd1a81ba5199fdfd34c3e66
sjanibekova <sdzhanybekova@seikolabs.kz>
2021-06-27 21:42:26 +0600

    asd

--- ssg/site.py
+++ ssg/site.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-
+import extensions
 
 class Site:
     def __init__(self, source, dest, parsers=None):
@@ -27,12 +27,14 @@ class Site:
             )
 
     def build(self):
+        extensions.load_bundle()
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+
 
     @staticmethod
     def error(message):
